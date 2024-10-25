2c30ecf67cb56d4e2f6a60b617e43b77ab8d685c
gianglamdangkhoa130904 <120297899+gianglamdangkhoa130904@users.noreply.github.com>
2024-10-25 22:40:39 +0700

    Delete clonegit/BKLand directory

--- clonegit/BKLand/backend/config.js
+++
@@ -1,4 +0,0 @@
-export const PORT = 1325
-
-export const mongoDBURL = 
-'mongodb+srv://root:admin123456@socialmediamern.geor3.mongodb.net/BKLand?retryWrites=true&w=majority&appName=SocialMediaMERN';

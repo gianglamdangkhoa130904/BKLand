606f28e8d2173d20cc74838dddb517df8ab7d298
gianglamdangkhoa130904 <120297899+gianglamdangkhoa130904@users.noreply.github.com>
2024-11-24 12:21:35 +0700

    Update ReturnPayment.jsx

--- BKLand/frontend/src/pages/customer/ReturnPayment.jsx
+++ BKLand/frontend/src/pages/customer/ReturnPayment.jsx
@@ -54,7 +54,7 @@ const ReturnPayment = () => {
       axios.put(`https://bkland.onrender.com/apartments/updateStatus/${apartment}`, dataAparment)
       .then((response) => {
         // console.log(response.data.data);
-        await fetchOrderDetails()
+        fetchOrderDetails()
       })
     })
   }
@@ -81,7 +81,7 @@ const ReturnPayment = () => {
         axios.post('https://bkland.onrender.com/certificates', dataOwnerShipCertificate)
         .then((response) => {
           console.log(response.data);
-          await fetchOrderDetails()
+          fetchOrderDetails()
         })
       })
     })

9f1a0d7a4c8dbf43461bef052965faf36277e116
gianglamdangkhoa130904 <120297899+gianglamdangkhoa130904@users.noreply.github.com>
2025-03-16 21:03:29 +0700

    Update InvoiceList.jsx

--- BKLand/frontend/src/pages/admin/InvoiceList.jsx
+++ BKLand/frontend/src/pages/admin/InvoiceList.jsx
@@ -174,7 +174,7 @@ function InvoiceList() {
               >
                 {customers.map((customer) => (
                   <option key={customer._id} value={customer._id}>
-                    {customer.name} - {customer.username} ({customer._id})
+                    {customer.name} - {customer.username} ({(customer._id).Substring(0, 3) + "..." + (customer._id).Substring(customerId.Length - 3)})
                   </option>
                 ))}
               </Select>
